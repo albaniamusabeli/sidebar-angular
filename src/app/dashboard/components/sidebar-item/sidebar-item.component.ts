4c2efeb5a92078ff3a4ea631792bc9e312eee2b9
Albania Musabeli <musabelialbania@gmail.com>
2024-11-01 14:18:50 -0300

    feat: Configurar rutas con submenu en sidebar

--- src/app/dashboard/components/sidebar-item/sidebar-item.component.ts
+++ src/app/dashboard/components/sidebar-item/sidebar-item.component.ts
@@ -1,22 +1,31 @@
-import { Component, input } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { SidebarLink } from './sidebar-items-list';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar-item',
   standalone: true,
   imports: [
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
+    CommonModule
   ],
   templateUrl: './sidebar-item.component.html',
   styleUrl: './sidebar-item.component.scss'
 })
 export class SidebarItemComponent {
 
-  public icon = input<string>();
-  public path = input<string>();
+  public icon = input<string | undefined>();
+  public path = input<string | undefined>();
   public title = input<string>();
+  public submenu = input<SidebarLink[] | undefined>();
 
+  public toggleSubmenu = signal<boolean>(false);
 
+  onClickSubmenu(){
+    // el valor contrario del booleano
+    this.toggleSubmenu.set(!this.toggleSubmenu());
+  }
 
 }
--- src/app/dashboard/components/sidebar-item/sidebar-items-list.ts
+++ src/app/dashboard/components/sidebar-item/sidebar-items-list.ts
@@ -1,17 +1,56 @@
-export const sidebarLinks = [
+export interface SidebarLink {
+    path?: string;
+    icon?: string;
+    title: string;
+    submenu?: SidebarLink[];
+}
+
+export const sidebarLinks: SidebarLink[] = [
     {
-        path:'home',
-        icon:'lni lni-home-2',
-        title:'Home'
+        path: 'home',
+        icon: 'lni lni-home-2',
+        title: 'Home'
     },
     {
-        path:'calendar',
-        icon:'lni lni-calendar-days',
-        title:'Calendar'
+        path: 'calendar',
+        icon: 'lni lni-calendar-days',
+        title: 'Calendar'
     },
     {
-        path:'profile',
-        icon:'lni lni-user-4',
-        title:'Profile'
+        path: 'profile',
+        icon: 'lni lni-user-4',
+        title: 'Profile'
+    },
+    {
+        icon: 'lni lni-file-plus-circle',
+        title: 'Create',
+        submenu: [{
+            path: 'folder',
+            title: 'Folder'
+        },
+        {
+            path: 'document',
+            title: 'Document'
+        },
+        {
+            path: 'project',
+            title: 'Project'
+        }]
+    },
+    {
+        icon: 'lni lni-pen-to-square',
+        title: 'Todo List',
+        submenu: [{
+            path: 'work',
+            title: 'Work'
+        },
+        {
+            path: 'private',
+            title: 'Private'
+        },
+        {
+            path: 'coding',
+            title: 'Coding'
+        }]
     }
 ]
